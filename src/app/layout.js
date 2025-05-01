33894bab31e303bceb66bbc8858a91cea7b7b510
uiFramer <83536252+uiFramer@users.noreply.github.com>
2025-05-01 23:09:01 +0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,29 +7,29 @@ import { Toaster } from 'sonner'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'MemeVerse - Your Ultimate Meme Platform',
-  description: 'Explore, create, and share the best memes on the internet. Stay updated with trending memes and showcase your creativity.',
-  keywords: 'memes, funny, humor, meme generator, trending memes, share memes, internet culture',
-  author: 'MemeVerse Team',
+  title: 'MemesTR',
+  description: 'Türk meme topluluğu.',
+  keywords: 'memes, komik, mizah, meme şablonları, trending, meme paylaş, internet kültürü',
+  author: 'MemesTR',
   openGraph: {
-    title: 'MemeVerse - Your Ultimate Meme Platform',
-    description: 'Explore, create, and share the best memes on the internet. Stay updated with trending memes and showcase your creativity.',
-    url: 'https://www.memeverse.com',
+    title: 'MemesTR',
+    description: 'Türk meme topluluğu.',
+    url: 'https://memestr.netlify.app',
     type: 'website',
     images: [
       {
         url: 'https://www.memeverse.com/og-image.jpg',
         width: 1200,
         height: 630,
-        alt: 'MemeVerse Logo',
+        alt: 'MemesTR',
       },
     ],
   },
   twitter: {
     card: 'summary_large_image',
-    site: '@MemeVerse',
-    title: 'MemeVerse - Your Ultimate Meme Platform',
-    description: 'Join MemeVerse to explore and share the best memes trending on the internet!',
+    site: '@memestr',
+    title: 'MemesTR',
+    description: 'Türk meme topluluğu.',
     image: 'https://www.memeverse.com/twitter-image.jpg',
   },
 };
