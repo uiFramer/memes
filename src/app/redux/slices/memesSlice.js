00e2b85d5e20ea4871db85db33eb8b439fb21936
uiFramer <83536252+uiFramer@users.noreply.github.com>
2025-05-02 04:14:48 +0300

    Update memesSlice.js

--- src/app/redux/slices/memesSlice.js
+++ src/app/redux/slices/memesSlice.js
@@ -4,7 +4,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
 export const fetchMemes = createAsyncThunk('memes/fetchMemes', async () => {
-  const response = await axios.get('https://memestr.netlify.app/explorer')
+  const response = await axios.get('memes')
   return response.data.data.memes
 })
 
