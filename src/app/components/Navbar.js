e6a57226ef644b17a6130a56ef40d12a0f17d162
uiFramer <83536252+uiFramer@users.noreply.github.com>
2025-05-03 11:57:53 +0300

    Update Navbar.js

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -32,9 +32,7 @@ export default function Navbar() {
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <Link href="/" className="flex items-center space-x-2">
-            <span className="text-2xl font-bold text-primary">
-            <img src="https://i.hizliresim.com/irt6ev4.png" height="30" width="30"/>
-            </span>
+            <span className="text-2xl font-bold text-primary">😂</span>
           </Link>
 
           {/* Desktop Navigation */}
